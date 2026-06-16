4d33766a70cb3863566b06a23630f0b1ed7f4ec0
danielkinsner <dannytownkins@gmail.com>
2026-06-16 08:32:40 -0700

    refactor(iphone): delete dead playback controller

--- apps/iphone-native/YESMasterNative/AudioSessionController.swift
+++
@@ -1,9 +0,0 @@
-import AVFoundation
-
-struct AudioSessionController {
-    func activateForPlayback() throws {
-        let session = AVAudioSession.sharedInstance()
-        try session.setCategory(.playback, mode: .default, options: [])
-        try session.setActive(true)
-    }
-}
