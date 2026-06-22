64a9969b8a91efa2312ca83b69fd04cea2d63bee
danielkinsner <dannytownkins@gmail.com>
2026-06-22 10:22:48 -0700

    feat: route browser to landing page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./App.css";
+import { isTauri } from "./lib/tauri-runtime";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function wantsAppShell(): boolean {
+  if (isTauri()) return true;
+  const url = new URL(window.location.href);
+  return url.pathname === "/app" || url.searchParams.has("app");
+}
+
+async function boot() {
+  const root = ReactDOM.createRoot(document.getElementById("root")!);
+  if (wantsAppShell()) {
+    await import("./App.css");
+    const { default: App } = await import("./App");
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    );
+    return;
+  }
+
+  await import("./LandingPage.css");
+  const { default: LandingPage } = await import("./LandingPage");
+  root.render(
+    <React.StrictMode>
+      <LandingPage />
+    </React.StrictMode>,
+  );
+}
+
+void boot();
